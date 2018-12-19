aca8c44aff9bc921387ce897dfd43c4b178b16d3
Anthony Agyekum Mensah <anthony@smsgh.com>
2018-12-19 01:14:33 +0000

    instagram filters added

--- src/app/selfie/selfie.component.ts
+++ src/app/selfie/selfie.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
+import {st} from '@angular/core/src/render3';
 
 
 @Component({
@@ -8,20 +9,58 @@ import {Component, OnInit, ViewChild} from '@angular/core';
 })
 export class SelfieComponent implements OnInit {
   @ViewChild('selfie') selfie: any;
+  @ViewChild('staging') staging: any;
   video: any;
   displayControls: any;
   imageReady: boolean;
   imageURL: string;
+  filterList: any;
+  filters: any;
+  appliedFilter: any;
+
 
   constructor() {
   }
 
   ngOnInit() {
     this.video = this.selfie.nativeElement;
+    this.getFilters();
+    this.startCamera();
+
+  }
+
+  getFilters() {
+    const stylesheets = document.styleSheets;
+    this.filterList = stylesheets[1].rules;
+    this.filters = [];
+    console.log('filter list: ', this.filterList);
+    for (let i = 0; i < this.filterList.length; i++) {
+      if (this.filterList[i].selectorText.includes('.filter-') && !this.filterList[i].selectorText.includes('before')) {
+        this.filters.push(this.filterList[i].selectorText);
+      }
+    }
+
+    for (let i = 0; i < this.filters.length; i++) {
+
+      this.filters[i] = this.filters[i].replace('.', '');
+
+    }
+
+    console.log('stylesheet : ', this.filters);
+  }
+
+  selectFilter(event) {
+    console.log(event.srcElement.value)
+    this.appliedFilter = event.srcElement.value;
+  }
+
+  startCamera() {
+    const config = {video: true, audio: false};
+    this.initCamera(config);
   }
 
   initCamera(config: any) {
-    var browser = <any>navigator;
+    const browser = <any>navigator;
 
     browser.getUserMedia = (browser.getUserMedia ||
       browser.webkitGetUserMedia ||
@@ -47,24 +86,22 @@ export class SelfieComponent implements OnInit {
     this.video.pause();
   }
 
-  toggleControls() {
-    this.video.controls = this.displayControls;
-    this.displayControls = !this.displayControls;
-  }
 
   resume() {
     this.video.play();
   }
 
   takePhoto() {
-    console.log('>>>')
-    var canvas = document.createElement('canvas');
-    canvas.width = 640;
-    canvas.height = 480;
+    console.log('>>>');
+    let canvas = document.createElement('canvas');
+    console.log('staging', this.staging.nativeElement.offsetWidth)
+const canvasWidth = this.staging.nativeElement.offsetWidth;
+    canvas.width = canvasWidth;
+    canvas.height = 500;
     var ctx = canvas.getContext('2d');
     //draw image to canvas. scale to target dimensions
     ctx.drawImage(this.video, 0, 0, canvas.width, canvas.height);
-this.imageReady = true;
+    this.imageReady = true;
 //convert to desired file format
     this.imageURL = canvas.toDataURL('image/jpeg'); // can also use 'image/png'
     // console.log('data url: ', dataURI);
